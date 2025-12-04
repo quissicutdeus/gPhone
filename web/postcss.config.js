d8e604dd5a9a21b9b76ebb089adbad8be673297d
quissicutdeus <github@quissicutdeus.com>
2025-12-03 22:10:56 -0800

    Add the postcss-preset-env package and configuration to convert away from the oklab format

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,6 +1,12 @@
 export default {
     plugins: {
         '@tailwindcss/postcss': {},
+        'postcss-preset-env': {
+            features: {
+                'oklab-function': { preserve: true },
+                'color-functional-notation': { preserve: true },
+            }
+        },
         autoprefixer: {},
     },
 }
